7b2d9abf9935dba9bf3680275cc307c147f18e55
lauriantus <lauriantus@gmail.com>
2022-06-09 22:47:08 +0300

    testing function

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-let a=100;
-let b;
-b=a+1;
-console.log(b);
-console.log("Круто");
-a=10;
-console.log(b)
+function multiply(num1,num2) {
+  var result = num1 * num2;
+  return result;
+}
+console.log(multiply(4,7));
+console.log(multiply(20,20));
+console.log(multiply("Kova",5));
+console.log(multiply(0.5,3));
