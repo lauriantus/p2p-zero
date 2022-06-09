8b09f5e439432fc1c3ebda7247f2351630c90403
lauriantus <lauriantus@gmail.com>
2022-06-09 22:58:44 +0300

    Testing
    function;
    event -onclick;
    add images for project

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 function multiply(num1,num2) {
   var result = num1 * num2;
   return result;
@@ -6,3 +7,11 @@ console.log(multiply(4,7));
 console.log(multiply(20,20));
 console.log(multiply("Kova",5));
 console.log(multiply(0.5,3));
+*/
+
+//test 2:events 
+/*
+document.querySelector("html").onclick = function () {
+    alert("Ouch! Stop poking me!")
+}
+*/
