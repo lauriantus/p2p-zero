99e992b0d8fa2cce9ffc4593aab2229d0495bc64
lauriantus <lauriantus@gmail.com>
2022-06-09 23:37:33 +0300

    tests
    event
    img
    auth

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//test 1:function:
 /*
 function multiply(num1,num2) {
   var result = num1 * num2;
@@ -9,9 +10,42 @@ console.log(multiply("Kova",5));
 console.log(multiply(0.5,3));
 */
 
-//test 2:events 
+//test 2:events:
 /*
 document.querySelector("html").onclick = function () {
     alert("Ouch! Stop poking me!")
 }
 */
+
+//test 3:onclick for image(change):
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === "./images/firefox_png_1.png") {
+        myImage.setAttribute('src', "./images/firefox_png_2.png");
+    } else {
+        myImage.setAttribute('src', "./images/firefox_png_1.png");
+}
+}
+
+//test 4: auth
+let myButton = document.querySelector(".button"),
+    myHeading = document.querySelector(".title_auth");
+
+function setUserName() {
+    let myName = prompt("Please, enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "JS is cool, " + myName;
+}
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "JS is cool, " + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
+}
