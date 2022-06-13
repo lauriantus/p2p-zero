a37dd0f1d9aed3d3e8a16aaba71b66b58fe0803d
lauriantus <lauriantus@gmail.com>
2022-06-13 22:40:11 +0300

    "Найбільше парне число"

---
+++ task14.js
@@ -0,0 +1,20 @@
+let bigger = -Infinity,
+  number;
+for (;;) {
+  let number = parseInt(
+    prompt(
+      "Якщо завершили введення чисел - введіть 0. Введіть число для порівняння:"
+    )
+  );
+  if (number === 0) {
+    break;
+  }
+  if (number % 2 === 0 && number > bigger) {
+    bigger = number;
+  }
+}
+if (bigger === -Infinity) {
+  console.log("Нажаль жодне число не було порівняно");
+} else {
+  console.log("bigger number: " + bigger);
+}
