acf2aa224482883d715c534dbe889f04a6c4e12d
lauriantus <lauriantus@gmail.com>
2022-06-13 22:41:40 +0300

    Ялинка
    Completed task and formatted for me

--- tasks/task12_revu.js
+++ tasks/task12_revu.js
@@ -1,16 +1,16 @@
 // Рибак Іван
 let height = +prompt("Hight: ");
 // колличество рядов елки
-for ( let i = 0; i <= height; i++){
-    for (let side = 0; side < height - i; side++) {
-        console.put(" ");
-    }
-    for (let branch = 0; branch < 2*i-1; branch++){
-        console.put("*");
-    } 
-    console.log( "  ");
-}
-for (let bottom = 0; bottom <= height - 2; bottom++){
+for (let i = 0; i <= height; i++) {
+  for (let side = 0; side < height - i; side++) {
     console.put(" ");
+  }
+  for (let branch = 0; branch < 2 * i - 1; branch++) {
+    console.put("*");
+  }
+  console.log("  ");
+}
+for (let bottom = 0; bottom <= height - 2; bottom++) {
+  console.put(" ");
 }
 console.put("*");
