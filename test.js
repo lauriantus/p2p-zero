7b2a2f3a0e4237831ecc6cdb9cdc1f9edffa3a45
lauriantus <lauriantus@gmail.com>
2022-06-24 23:39:15 +0300

    new

---
+++ test.js
@@ -0,0 +1,8 @@
+let str = "123";
+console.log(typeof str);
+str = +str;
+console.log(typeof str);
+let a = +prompt("Enter some num");
+console.log(typeof a);
+let b = +prompt("Enter some num");
+console.log(typeof b);
